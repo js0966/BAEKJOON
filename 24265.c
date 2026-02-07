04c568c186b62d1febdeff16dec41b85b6e2efea
js0966 <jj03120705@gmail.com>
2026-02-07 21:29:54 +0900

    Solve BOJ 24265

---
+++ 24265.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+    long long n;
+    scanf("%lld", &n);
+
+    printf("%lld\n", n * (n - 1) / 2);
+    printf("2\n");
+    
+    return 0;
+}
