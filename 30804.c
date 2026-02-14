399d158f6e690e046551faff8f995329dc26b1a9
js0966 <jj03120705@gmail.com>
2026-02-14 15:32:15 +0900

    Solve BOJ 30804

---
+++ 30804.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+#define MAX 200000
+
+int array[MAX];
+
+int main(void) {
+    int N;
+    scanf("%d", &N);
+
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &array[i]);
+    }
+
+    int L = 0;
+    int R = N - 1; // last of array index
+
+    for (int i = 0; i < N; i++) {
+
+    }
+}
